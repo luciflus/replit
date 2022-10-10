a20cc692f55a85eb498367052f88e7f7e47ceac0
Sultan Usmanov <sultanu@inbox.ru>
2022-10-10 17:18:33 +0000

    hidden password

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-int
+username = input('What is your username? ')
+password = input('What is your password? ')
+password_lenght = len(password)
+hidden_password = '*' * password_lenght
+
+print(f'{username}, your password, {hidden_password}, length is {password_lenght}')
